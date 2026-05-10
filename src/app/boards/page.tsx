962a3672e2dada2e789de12217304d84ded6f6a2
priyanshu101120 <gs093863@gmail.com>
2026-05-10 09:55:41 +0530

    fix boards

--- src/app/boards/page.tsx
+++ src/app/boards/page.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import BoardNav from "../../components/boards/BoardNav";
+
 import Boards from "@/components/boards/Boards";
 
 const boards = () => {
   return (
     <>
-      {/* <BoardNav /> */}
+      
       <Boards />
     </>
   );
