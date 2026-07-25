ac6a71cb1dfeb463c2b84db17ad809e9d996a72a
priyanshu101120 <gs093863@gmail.com>
2026-07-25 09:44:41 +0530

    fix send emailOtp

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -1,10 +1,9 @@
-// src/utils/sendEmail.js
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 465,         
-  secure: true,      
+  port: 587,         
+  secure: false,      
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
@@ -12,6 +11,7 @@ const transporter = nodemailer.createTransport({
   tls: {
     rejectUnauthorized: false,
   },
+  family: 4, 
 });
 
 const sendEmail = async ({ email, subject, otp }) => {
